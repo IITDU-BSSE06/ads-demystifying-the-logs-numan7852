2e58d77a28da1d6d1646630b30a8f0dd8eda0ff8
numan7852 <bsse0624@iit.du.ac.bd>
2017-11-07 18:28:41 +0600

    Assignment 5 (reducer.py)
    How many unique files are there?

---
+++ assignment5/reducer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+
+path = {}
+
+for line in sys.stdin:
+	file_hits = line.strip()	
+
+	if file_hits in path:
+		path[file_hits] += 1
+	else:
+		path[file_hits] = 1
+
+print len(path)
