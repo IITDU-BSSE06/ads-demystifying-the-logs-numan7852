43dd77e065b4106c92be17a1e31e0463485a8c51
numan7852 <bsse0624@iit.du.ac.bd>
2017-11-08 12:52:18 +0600

    Update reducer.py

---
+++ assignment7/reducer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+path = {}
+
+for line in sys.stdin:
+	file_hits = line.strip()	
+
+	if file_hits in path:
+		path[file_hits] += 1
+	else:
+		path[file_hits] = 1
+
+count, max_hits_path = path.values(), path.keys()
+print max_hits_path, " " , count
